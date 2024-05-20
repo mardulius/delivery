5f46ae7d539528fe1e6d56d0e977740c56a33e3b
Dmitry Karpunin <kotel77@inbox.ru>
2024-05-20 12:41:32 +0300

    adding shared kernel Value Objects and tests

---
+++ DeliveryApp.Core/Domain/SharedKernel/Weight.cs
@@ -0,0 +1,53 @@
+﻿using CSharpFunctionalExtensions;
+using Primitives;
+
+namespace DeliveryApp.Core.Domain.SharedKernel
+{
+    public class Weight : ValueObject
+    {
+        public int Value { get; }
+        private Weight()
+        {
+        }
+
+        private Weight(int value)
+        {
+            Value = value;
+        }
+
+        public static Result<Weight, Error> Create(int value)
+        {
+            if (value <= 0) return GeneralErrors.ValueIsRequired(nameof(value));
+            return new Weight(value);
+        }
+
+        public static bool operator <(Weight first, Weight second)
+        {
+            var result = first.Value < second.Value;
+            return result;
+        }
+
+        public static bool operator >(Weight first, Weight second)
+        {
+            var result = first.Value > second.Value;
+            return result;
+        }
+
+        public static bool operator <=(Weight first, Weight second)
+        {
+            var result = first.Value <= second.Value;
+            return result;
+        }
+
+        public static bool operator >=(Weight first, Weight second)
+        {
+            var result = first.Value >= second.Value;
+            return result;
+        }
+
+        protected override IEnumerable<IComparable> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
